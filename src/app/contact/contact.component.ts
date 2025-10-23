a5707e264a7082e124a401a70b1a699433e97f95
Ateba Basile <atebabasile@gmail.com>
2025-10-23 02:36:16 +0200

    push test 1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Ombre } from '../model/ombre.model.component';
+import { ListCardComponent } from './../list-card/list-card.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ListCardComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+   ombre1! : Ombre;
+    constructor(){
+      this.ombre1 = new Ombre();
+      this.ombre1.name = 'igris';
+      this.ombre1.figureName = 'Commandant rouge-sang Domaine du monarque';
+      this.ombre1.classe = 'classe S';
+      this.ombre1.rang = 'Maréchal';
+    }
 }
